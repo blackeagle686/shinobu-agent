25841b01933ae9394a28469cd98f7389462177de
blackeagle686 <mathematecs1@gmail.com>
2026-05-06 12:23:07 +0300

    Added test script for DuckDuckGo search

---
+++ scratch/test_ddgs.py
@@ -0,0 +1,6 @@
+from duckduckgo_search import DDGS
+import json
+
+with DDGS() as ddgs:
+    results = [r for r in ddgs.text("python programming", max_results=5)]
+    print(json.dumps(results, indent=2))
