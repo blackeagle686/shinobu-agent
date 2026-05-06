2df68249fe5860282c6ed20157fe73624c41a5c7
blackeagle686 <mathematecs1@gmail.com>
2026-05-06 03:40:12 +0300

    Removed unused brain module imports

--- shinobu/cognition/brains/__init__.py
+++
@@ -1,6 +0,0 @@
-from .thinker import ShinobuThinker
-from .planner import ShinobuPlanner
-from .reflector import ShinobuReflector
-from .generator import ShinobuGenerator
-
-__all__ = ["ShinobuThinker", "ShinobuPlanner", "ShinobuReflector", "ShinobuGenerator"]
